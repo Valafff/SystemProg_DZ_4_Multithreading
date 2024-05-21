c7cd9ff15ae6cee2e26d6bcfa6279fc5cec3e63c
Vetal <vvvv@vvvv.vv>
2024-05-21 21:57:21 +0300

    Add project files.

---
+++ SystemProg_DZ_4_Multithreading/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SystemProg_DZ_4_Multithreading
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown_NumberBars = new System.Windows.Forms.NumericUpDown();
+			this.bt_Start = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_NumberBars)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(199, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Введите количество прогресс-баров: ";
+			// 
+			// numericUpDown_NumberBars
+			// 
+			this.numericUpDown_NumberBars.Location = new System.Drawing.Point(218, 6);
+			this.numericUpDown_NumberBars.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown_NumberBars.Name = "numericUpDown_NumberBars";
+			this.numericUpDown_NumberBars.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown_NumberBars.TabIndex = 1;
+			this.numericUpDown_NumberBars.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// bt_Start
+			// 
+			this.bt_Start.Location = new System.Drawing.Point(344, 3);
+			this.bt_Start.Name = "bt_Start";
+			this.bt_Start.Size = new System.Drawing.Size(151, 23);
+			this.bt_Start.TabIndex = 2;
+			this.bt_Start.Text = "Создать и запустить";
+			this.bt_Start.UseVisualStyleBackColor = true;
+			this.bt_Start.Click += new System.EventHandler(this.bt_Start_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(515, 450);
+			this.Controls.Add(this.bt_Start);
+			this.Controls.Add(this.numericUpDown_NumberBars);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Танцующие прогресс-бары";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_NumberBars)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown_NumberBars;
+		private System.Windows.Forms.Button bt_Start;
+	}
+}
+
---
+++ SystemProg_DZ_4_Multithreading/Form1.cs
@@ -0,0 +1,96 @@
+﻿//Work in progress
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SystemProg_DZ_4_Multithreading
+{
+	public partial class Form1 : Form
+	{
+		
+		//List<ProgressBar> progressBarList = new List<ProgressBar>();
+
+
+		BackgroundWorker bgw = new BackgroundWorker();
+		CustomProgressBar tempProgressBar = new CustomProgressBar();
+		Random random = new Random();
+		public Form1()
+		{
+			InitializeComponent();
+
+			//ProgressBar temp = new ProgressBar();
+			tempProgressBar.Location = new Point(100, 200);
+			tempProgressBar.Height = 20;
+			tempProgressBar.Width = 200;
+			tempProgressBar.Minimum = 0;
+			tempProgressBar.Maximum = 100;
+			Controls.Add(tempProgressBar);
+		}
+
+		private void bt_Start_Click(object sender, EventArgs e)
+		{
+			bgw.DoWork += new DoWorkEventHandler(bgw_DoWork);
+			bgw.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
+			bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+			bgw.WorkerReportsProgress = true;
+			bgw.RunWorkerAsync();
+		}
+
+		void bgw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			int total = 57; 
+			for (int i = 0; i <= total; i++)
+			{
+				System.Threading.Thread.Sleep(100); 
+				int percent = (i * 100) / total;
+				bgw.ReportProgress(percent, i);
+			}
+		}
+
+		void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{ 
+			tempProgressBar.Value = e.ProgressPercentage;
+			//label1.Text = $"Прогресс: {e.ProgressPercentage}%";
+			//label2.Text = $"Передано элементов: {e.UserState}";
+		}
+
+		void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			// Завершение работы фонового потока
+		}
+
+		//private void Form1_Load(object sender, EventArgs e)
+		//{
+		//	CustomProgressBar customProgressBar = new CustomProgressBar();
+		//	customProgressBar.Location = new Point(10, 10);
+		//	customProgressBar.Size = new Size(200, 20);
+		//	Controls.Add(customProgressBar);
+		//}
+	}
+
+	public class CustomProgressBar : System.Windows.Forms.ProgressBar
+	{
+		public CustomProgressBar()
+		{
+			SetStyle(ControlStyles.UserPaint, true);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			Rectangle rect = ClientRectangle;
+			rect.Width = (int)(rect.Width * ((double)Value / Maximum)) - 4;
+			e.Graphics.FillRectangle(Brushes.Red, 2, 2, rect.Width, rect.Height - 4);
+		}
+	}
+
+	// Использование в форме:
+
+}
