ed8527641e9eeb393a643c899949f2fdab7fdd85
Vetal <vvvv@vvvv.vv>
2024-05-22 21:44:18 +0300

    add multibars

--- SystemProg_DZ_4_Multithreading/Form1.Designer.cs
+++ SystemProg_DZ_4_Multithreading/Form1.Designer.cs
@@ -31,58 +31,94 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.numericUpDown_NumberBars = new System.Windows.Forms.NumericUpDown();
 			this.bt_Start = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_NumberBars)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(199, 13);
+			this.label1.Size = new System.Drawing.Size(165, 40);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Введите количество прогресс-баров: ";
 			// 
 			// numericUpDown_NumberBars
 			// 
-			this.numericUpDown_NumberBars.Location = new System.Drawing.Point(218, 6);
+			this.numericUpDown_NumberBars.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.numericUpDown_NumberBars.Location = new System.Drawing.Point(174, 3);
 			this.numericUpDown_NumberBars.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
 			this.numericUpDown_NumberBars.Name = "numericUpDown_NumberBars";
-			this.numericUpDown_NumberBars.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown_NumberBars.Size = new System.Drawing.Size(165, 20);
 			this.numericUpDown_NumberBars.TabIndex = 1;
 			this.numericUpDown_NumberBars.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+			this.numericUpDown_NumberBars.ValueChanged += new System.EventHandler(this.numericUpDown_NumberBars_ValueChanged);
 			// 
 			// bt_Start
 			// 
-			this.bt_Start.Location = new System.Drawing.Point(344, 3);
+			this.bt_Start.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.bt_Start.Location = new System.Drawing.Point(345, 3);
 			this.bt_Start.Name = "bt_Start";
-			this.bt_Start.Size = new System.Drawing.Size(151, 23);
+			this.bt_Start.Size = new System.Drawing.Size(167, 34);
 			this.bt_Start.TabIndex = 2;
 			this.bt_Start.Text = "Создать и запустить";
 			this.bt_Start.UseVisualStyleBackColor = true;
 			this.bt_Start.Click += new System.EventHandler(this.bt_Start_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+			this.tableLayoutPanel1.Controls.Add(this.bt_Start, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown_NumberBars, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(515, 450);
+			this.tableLayoutPanel1.TabIndex = 3;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 43);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(509, 404);
+			this.flowLayoutPanel1.TabIndex = 3;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(515, 450);
-			this.Controls.Add(this.bt_Start);
-			this.Controls.Add(this.numericUpDown_NumberBars);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Form1";
 			this.Text = "Танцующие прогресс-бары";
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_NumberBars)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -91,6 +127,8 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.NumericUpDown numericUpDown_NumberBars;
 		private System.Windows.Forms.Button bt_Start;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 	}
 }
 
--- SystemProg_DZ_4_Multithreading/Form1.cs
+++ SystemProg_DZ_4_Multithreading/Form1.cs
@@ -15,57 +15,124 @@ namespace SystemProg_DZ_4_Multithreading
 {
 	public partial class Form1 : Form
 	{
-		
+
 		//List<ProgressBar> progressBarList = new List<ProgressBar>();
+		int countBars;
 
-
-		BackgroundWorker bgw = new BackgroundWorker();
+		//BackgroundWorker bgw = new BackgroundWorker();
 		CustomProgressBar tempProgressBar = new CustomProgressBar();
+
 		Random random = new Random();
+		List<CustomProgressBar> progressBars = new List<CustomProgressBar>();
+
+
+
+
 		public Form1()
 		{
 			InitializeComponent();
 
 			//ProgressBar temp = new ProgressBar();
+			//tempProgressBar.Location = new Point(100, 200);
+			//tempProgressBar.Height = 20;
+			//tempProgressBar.Width = 200;
+			//tempProgressBar.Minimum = 0;
+			//tempProgressBar.Maximum = 100;
+			//flowLayoutPanel1.Controls.Add(tempProgressBar);
+		}
+
+		private void numericUpDown_NumberBars_ValueChanged(object sender, EventArgs e)
+		{
+			countBars = Convert.ToInt32(numericUpDown_NumberBars.Value);
+		}
+
+		public void NewProgressBar()
+		{
+			BackgroundWorker bgw = new BackgroundWorker();
+			CustomProgressBar tempProgressBar = new CustomProgressBar();
+
 			tempProgressBar.Location = new Point(100, 200);
 			tempProgressBar.Height = 20;
-			tempProgressBar.Width = 200;
+			tempProgressBar.Width = 450;
 			tempProgressBar.Minimum = 0;
 			tempProgressBar.Maximum = 100;
-			Controls.Add(tempProgressBar);
+			flowLayoutPanel1.Controls.Add(tempProgressBar);
+
+			bgw.DoWork += new DoWorkEventHandler(bgw_DoWork);
+			bgw.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
+			//bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+			bgw.WorkerReportsProgress = true;
+			bgw.RunWorkerAsync();
+
+			void bgw_DoWork(object sender, DoWorkEventArgs e)
+			{
+				int total = 100;
+				for (int i = 0; i <= total; i++)
+				{
+					System.Threading.Thread.Sleep(100);
+					int percent = (i * 100) / total;
+					bgw.ReportProgress(percent, i);
+				}
+			}
+
+			void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+			{
+				tempProgressBar.Value = e.ProgressPercentage;
+			}
+
+			//void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+			//{
+			//	// Завершение работы фонового потока
+			//}
+
+
 		}
+
+
+
+
+
+
 
 		private void bt_Start_Click(object sender, EventArgs e)
 		{
-			bgw.DoWork += new DoWorkEventHandler(bgw_DoWork);
-			bgw.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
-			bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
-			bgw.WorkerReportsProgress = true;
-			bgw.RunWorkerAsync();
-		}
-
-		void bgw_DoWork(object sender, DoWorkEventArgs e)
-		{
-			int total = 57; 
-			for (int i = 0; i <= total; i++)
+			for (int i = 0; i < countBars ; i++)
 			{
-				System.Threading.Thread.Sleep(100); 
-				int percent = (i * 100) / total;
-				bgw.ReportProgress(percent, i);
+				NewProgressBar();
 			}
+
+
+			//FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel();
+			//flowLayoutPanel.AutoSize = true;
+
+			//	System.Windows.Forms.Label some_lb = new System.Windows.Forms.Label();
+			//some_lb.AutoSize = true;
+			//some_lb.Text = (DateTime.Now).ToLongTimeString();
+			//flowLayoutPanel.Controls.Add(some_lb);
+
+			//System.Windows.Forms.Button btn_delete_lable_ = new System.Windows.Forms.Button();
+			//btn_delete_lable_.Text = "x";
+			//btn_delete_lable_.BackColor = Color.Red;
+
+			//btn_delete_lable_.Click += (object s, EventArgs ee) =>
+			//{
+			//	flowLayoutPanel1.Controls.Remove(flowLayoutPanel);
+			//};
+
+			//flowLayoutPanel.Controls.Add(btn_delete_lable_);
+
+			//flowLayoutPanel1.Controls.Add(flowLayoutPanel);
+
+
+			//bgw.DoWork += new DoWorkEventHandler(bgw_DoWork);
+			//bgw.ProgressChanged += new ProgressChangedEventHandler(bgw_ProgressChanged);
+			//bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+			//bgw.WorkerReportsProgress = true;
+			//bgw.RunWorkerAsync();
 		}
 
-		void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
-		{ 
-			tempProgressBar.Value = e.ProgressPercentage;
-			//label1.Text = $"Прогресс: {e.ProgressPercentage}%";
-			//label2.Text = $"Передано элементов: {e.UserState}";
-		}
 
-		void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-		{
-			// Завершение работы фонового потока
-		}
+
 
 		//private void Form1_Load(object sender, EventArgs e)
 		//{
