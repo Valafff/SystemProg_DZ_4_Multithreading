3373eae55905023dd084ff1eae0544d30b33b79a
Vetal <vvvv@vvvv.vv>
2024-05-24 18:03:27 +0300

    complete 2 task

---
+++ Horses/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Horses
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btn_Start = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.btn_Start, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// btn_Start
+			// 
+			this.btn_Start.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btn_Start.Location = new System.Drawing.Point(3, 3);
+			this.btn_Start.Name = "btn_Start";
+			this.btn_Start.Size = new System.Drawing.Size(794, 24);
+			this.btn_Start.TabIndex = 0;
+			this.btn_Start.Text = "Начать забег!";
+			this.btn_Start.UseVisualStyleBackColor = true;
+			this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 33);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 414);
+			this.flowLayoutPanel1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Скачки";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button btn_Start;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+	}
+}
+
---
+++ Horses/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Horses
+{
+	public partial class Form1 : Form
+	{
+		const int countHorses = 5;
+		Random random = new Random();
+		List<System.Windows.Forms.ProgressBar> Horses = new List<System.Windows.Forms.ProgressBar>();
+		public Form1()
+		{
+			InitializeComponent();
+			for (int i = 0; i < countHorses; i++)
+			{
+				Horses.Add( HorseGenerator(i+1));
+				flowLayoutPanel1.Controls.Add(Horses[i]);
+			}
+		}
+
+		public System.Windows.Forms.ProgressBar HorseGenerator(int number)
+		{
+			System.Windows.Forms.ProgressBar NewHorse = new System.Windows.Forms.ProgressBar();
+			NewHorse.Name = $"Лошадь № {number}";
+			NewHorse.Minimum = 0;
+			NewHorse.Maximum = 100;
+			NewHorse.Step = random.Next(1, 10);
+			NewHorse.Height = 20;
+			NewHorse.Width = 780;
+			return NewHorse;
+		}
+
+		private void btn_Start_Click(object sender, EventArgs e)
+		{
+			foreach (var item in Horses)
+			{
+				Thread thread = new Thread(new ParameterizedThreadStart(Go));
+				thread.Start(item);
+			}
+		}
+
+		public void Go(object bar)//заполнение прогресс бара
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				try
+				{
+					Invoke(new Action(() => (bar as System.Windows.Forms.ProgressBar).PerformStep()));
+				}
+				catch (Exception ex) { }
+				Thread.Sleep(500);
+				if ((bar as System.Windows.Forms.ProgressBar).Value == 100)//когда достигнуто полное заполнение добавляем лошадь в результирующую таблицу
+				{
+				
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
